a245a489dcbed33ba39e4c59e814f918ac553da1
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-16 16:12:06 +0530

    Initial Commit.

---
+++ Spring_Boot_CSRF_Token/src/main/java/com/example/Spring_Boot_CSRF_Token/SpringBootCsrfTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_CSRF_Token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCsrfTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCsrfTokenApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_CSRF_Token/src/test/java/com/example/Spring_Boot_CSRF_Token/SpringBootCsrfTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_CSRF_Token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCsrfTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
