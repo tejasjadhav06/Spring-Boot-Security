4b610fabf86fa4258b9934a953c09849fcfd6e10
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-16 15:55:34 +0530

    Intial Commit

---
+++ Spring_Boot_Custom_Login/src/main/java/com/example/Spring_Boot_Custom_Login/SpringBootCustomLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Custom_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomLoginApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Custom_Login/src/test/java/com/example/Spring_Boot_Custom_Login/SpringBootCustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Custom_Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
