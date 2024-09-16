0a3142b71836f74235c81497e5b3c7583a9a1150
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-16 16:40:07 +0530

    Generating csrf token.

--- Spring_Boot_CSRF_Token/src/main/java/com/example/Spring_Boot_CSRF_Token/controller/StudentController.java
+++ Spring_Boot_CSRF_Token/src/main/java/com/example/Spring_Boot_CSRF_Token/controller/StudentController.java
@@ -3,12 +3,17 @@ package com.example.Spring_Boot_CSRF_Token.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Spring_Boot_CSRF_Token.model.Student;
 import com.example.Spring_Boot_CSRF_Token.service.StudentService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class StudentController {
@@ -24,5 +29,15 @@ public class StudentController {
 	@GetMapping("/students")
 	public List<Student> getAllStudents(){
 		return service.getAllStudents();
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return service.getCsrfToken(request);
+	}
+	
+	@PostMapping("/students")
+	public void addStudent(@RequestBody Student student) {
+		service.addStudent(student);
 	}
 }
--- Spring_Boot_CSRF_Token/src/main/java/com/example/Spring_Boot_CSRF_Token/service/StudentService.java
+++ Spring_Boot_CSRF_Token/src/main/java/com/example/Spring_Boot_CSRF_Token/service/StudentService.java
@@ -1,19 +1,31 @@
 package com.example.Spring_Boot_CSRF_Token.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Service;
 
 import com.example.Spring_Boot_CSRF_Token.model.Student;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Service
 public class StudentService {
 
-	List<Student> students = Arrays.asList(new Student(101,"Tejas",89),
-			new Student(102,"Shubham",90));
+	List<Student> students = new ArrayList(Arrays.asList(new Student(101,"Tejas",89),
+			new Student(102,"Shubham",90)));
 	
 	public List<Student> getAllStudents(){
 		return students;
+	}
+
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
 	}
 }
