771319cf538a99de17c355df49abf2844030daeb
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-17 15:17:16 +0530

    Settings properties for database.

--- Spring_Custom_Configuration/src/main/java/com/example/Spring_Custom_Configuration/service/StudentService.java
+++ Spring_Custom_Configuration/src/main/java/com/example/Spring_Custom_Configuration/service/StudentService.java
@@ -2,6 +2,7 @@ package com.example.Spring_Custom_Configuration.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.Spring_Custom_Configuration.model.Student;
@@ -10,6 +11,7 @@ import com.example.Spring_Custom_Configuration.repository.StudentRepo;
 @Service
 public class StudentService {
 
+	@Autowired
 	private StudentRepo repo;
 	
 	public List<Student> getAllStudents(){
