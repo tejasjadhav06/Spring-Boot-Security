3eefd150547306adefade325e5113c8777e5304e
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-17 10:44:49 +0530

    Initial Commit.

---
+++ Spring_Custom_Configuration/src/main/java/com/example/Spring_Custom_Configuration/SpringCustomConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Custom_Configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCustomConfigurationApplication.class, args);
+	}
+
+}
---
+++ Spring_Custom_Configuration/src/test/java/com/example/Spring_Custom_Configuration/SpringCustomConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Custom_Configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
