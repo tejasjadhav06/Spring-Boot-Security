90589d7192eee9a831f36e17eeccc1b9823bf994
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-17 16:22:14 +0530

    Configure the custom settings.

---
+++ Spring_Custom_Configuration/src/main/java/com/example/Spring_Custom_Configuration/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.Spring_Custom_Configuration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	public SecurityFilterChain configSetting(HttpSecurity http) throws Exception {
+		return http
+				.csrf(customizer -> customizer.disable())
+				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.httpBasic(Customizer.withDefaults())
+				.build();
+	}
+}
