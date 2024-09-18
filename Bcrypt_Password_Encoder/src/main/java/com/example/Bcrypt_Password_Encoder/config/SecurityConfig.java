3fa03cf44013a67fa24fe04ba8a82b0dcd0819a3
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-18 14:55:05 +0530

    Encoded password of user try to login.

--- Bcrypt_Password_Encoder/src/main/java/com/example/Bcrypt_Password_Encoder/config/SecurityConfig.java
+++ Bcrypt_Password_Encoder/src/main/java/com/example/Bcrypt_Password_Encoder/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,7 +34,7 @@ public class SecurityConfig {
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		provider.setUserDetailsService(userDetailsService);
 		return provider;
 	}
