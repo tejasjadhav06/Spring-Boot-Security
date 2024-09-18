815b176045e9169029627bec080bcc58fdce8cfb
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-18 14:15:20 +0530

    Initial Commit.

---
+++ Bcrypt_Password_Encoder/src/main/java/com/example/Bcrypt_Password_Encoder/BcryptPasswordEncoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bcrypt_Password_Encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcryptPasswordEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcryptPasswordEncoderApplication.class, args);
+	}
+
+}
---
+++ Bcrypt_Password_Encoder/src/test/java/com/example/Bcrypt_Password_Encoder/BcryptPasswordEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bcrypt_Password_Encoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcryptPasswordEncoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
