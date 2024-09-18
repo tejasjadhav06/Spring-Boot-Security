d0d93c2ee2b45c7caf4d757011975202b05de4a8
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-18 12:06:26 +0530

    Creating a custom authentication.

--- Verify_User/src/main/java/com/example/Verify_User/config/SecurityConfig.java
+++ Verify_User/src/main/java/com/example/Verify_User/config/SecurityConfig.java
@@ -2,16 +2,21 @@ package com.example.Verify_User.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private UserDetailsService userDetailsService; 
+	
 	@Bean
 	public SecurityFilterChain setSettings(HttpSecurity http) throws Exception {
 		
@@ -21,5 +26,13 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.httpBasic(Customizer.withDefaults())
 				.build();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setUserDetailsService(userDetailsService);
+		return provider;
 	}
 }
