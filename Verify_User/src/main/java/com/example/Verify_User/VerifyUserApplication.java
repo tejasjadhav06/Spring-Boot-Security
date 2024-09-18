e77da6ec6a369e0ce31e7390b491257460aff10d
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-18 11:06:28 +0530

    Initial Commit.

---
+++ Verify_User/src/main/java/com/example/Verify_User/VerifyUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.Verify_User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyUserApplication.class, args);
+	}
+
+}
