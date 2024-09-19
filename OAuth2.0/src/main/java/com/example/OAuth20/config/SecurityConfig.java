927d2ce2a9e3641d714fa4919acb9aeb36d9e51d
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-19 10:30:58 +0530

    Setting up a custom security filter chain.

---
+++ OAuth2.0/src/main/java/com/example/OAuth20/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.OAuth20.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain setSettings(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+				.oauth2Client(Customizer.withDefaults())
+				.build();
+	}
+}
