c640d0783adc7b075945b42b8326d3816a48fba1
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-19 12:00:38 +0530

    Adding a filter layer.

--- JWT_Token/src/main/java/com/example/JWT_Token/config/SecurityConfig.java
+++ JWT_Token/src/main/java/com/example/JWT_Token/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.JWT_Token.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,5 +37,11 @@ public class SecurityConfig {
 		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 		provider.setUserDetailsService(userDetailsService);
 		return provider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration
+			config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 }
