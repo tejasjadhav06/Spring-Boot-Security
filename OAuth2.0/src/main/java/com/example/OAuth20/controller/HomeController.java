a2c3fbfe16180f00c8e56581709ba829d3a1c7b9
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-19 10:30:33 +0530

    Create a controller to handle the request.

---
+++ OAuth2.0/src/main/java/com/example/OAuth20/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.OAuth20.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "This is a Home Page of Our Application";
+	}
+}
