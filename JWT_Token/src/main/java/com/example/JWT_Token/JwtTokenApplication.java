2e67a7ee21efffd5d10a71da21533c93fa3c4fe8
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-19 11:35:42 +0530

    Initial Commit.

---
+++ JWT_Token/src/main/java/com/example/JWT_Token/JwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_Token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenApplication.class, args);
+	}
+
+}
