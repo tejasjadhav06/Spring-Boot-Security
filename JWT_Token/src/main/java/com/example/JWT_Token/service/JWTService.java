a6738f34c8bd2b1b91888890406a306b645e10e1
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-20 11:17:44 +0530

    Extracting username and validating token.

--- JWT_Token/src/main/java/com/example/JWT_Token/service/JWTService.java
+++ JWT_Token/src/main/java/com/example/JWT_Token/service/JWTService.java
@@ -6,6 +6,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -13,6 +14,7 @@ import javax.crypto.SecretKey;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -47,16 +49,38 @@ public class JWTService {
 				.compact();
 	}
 
-	private Key getKey() {
-		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-		return Keys.hmacShaKeyFor(keyBytes);
-	}
+	 private SecretKey getKey() {
+	        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+	        return Keys.hmacShaKeyFor(keyBytes);
+	    }
 
-	public String extractUserName(String token) {
-		return "";
-	}
+	    public String extractUserName(String token) {
+	        return extractClaim(token, Claims::getSubject);
+	    }
 
-	public boolean validateToken(String token, UserDetails userDetails) {
-		return false;
-	}
+	    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+	        final Claims claims = extractAllClaims(token);
+	        return claimResolver.apply(claims);
+	    }
+
+	    private Claims extractAllClaims(String token) {
+	        return Jwts.parser()
+	                .verifyWith(getKey())
+	                .build()
+	                .parseSignedClaims(token)
+	                .getPayload();
+	    }
+
+	    public boolean validateToken(String token, UserDetails userDetails) {
+	        final String userName = extractUserName(token);
+	        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	    }
+
+	    private boolean isTokenExpired(String token) {
+	        return extractExpiration(token).before(new Date());
+	    }
+
+	    private Date extractExpiration(String token) {
+	        return extractClaim(token, Claims::getExpiration);
+	    }
 }
