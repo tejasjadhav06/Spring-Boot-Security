1044d75be673e9246dff170a9dbf07f30a55cb41
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-20 10:59:12 +0530

    Adding a jwt filter before usernamepasswordfilter.

--- JWT_Token/src/main/java/com/example/JWT_Token/config/SecurityConfig.java
+++ JWT_Token/src/main/java/com/example/JWT_Token/config/SecurityConfig.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +22,9 @@ public class SecurityConfig {
 
 	@Autowired
 	private UserDetailsService userDetailsService; 
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Bean
 	public SecurityFilterChain setSettings(HttpSecurity http) throws Exception {
@@ -32,6 +37,7 @@ public class SecurityConfig {
 						.anyRequest().authenticated())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.httpBasic(Customizer.withDefaults())
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 	
