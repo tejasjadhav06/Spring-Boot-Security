eb852a149d153540509c11d9a583eff7a67a6612
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-20 11:27:07 +0530

    Add Component annotation.

--- JWT_Token/src/main/java/com/example/JWT_Token/config/JwtFilter.java
+++ JWT_Token/src/main/java/com/example/JWT_Token/config/JwtFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.JWT_Token.service.JWTService;
@@ -18,6 +19,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter{
 
 	@Autowired
